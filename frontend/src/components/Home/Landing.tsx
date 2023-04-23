1d32f0c3d724de859aa2da6a05ab8d17be519f56
sohanagashimani <sohanagashimanii@gmail.com>
2023-04-24 00:28:26 +0530

    Add: election guidelines and results page.

--- frontend/src/components/Home/Landing.tsx
+++ frontend/src/components/Home/Landing.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "../../axios";
+import Spinner from "../Spinner";
 
 const Landing = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [status, setStatus] = useState<"not-started" | "running" | "finished">(
+    "not-started"
+  );
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("/polls/status")
+      .then((res) => {
+        setStatus(res.data.status);
+        setLoading(false);
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }, []);
+
+  if (loading)
+    return (
+      <div className="loading-container">
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <Spinner spinning={true} />
+        </div>
+      </div>
+    );
   return (
     <div className="landing">
       <div className="left">
@@ -18,9 +45,19 @@ const Landing = () => {
             <button className="button-black">Login</button>
           </Link>
 
-          <Link to="/view">
-            <button style={{ marginLeft: "1rem" }}>View Votes</button>
+          <Link to="/votingGuidelines">
+            <button style={{ marginLeft: "1rem" }}>Voting Guidelines</button>
           </Link>
+          {status === "finished" && (
+            <Link
+              to={{
+                pathname: "/results",
+                search: `?status=${status}`,
+              }}
+            >
+              <button style={{ marginLeft: "1rem" }}>Results</button>
+            </Link>
+          )}
         </div>
       </div>
 
--- frontend/src/pages/Start.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Waiting from "../components/Waiting";
-
-const Start = () => {
-  return <Waiting title={"in progress"} />;
-  
-};
-
-export default Start;
